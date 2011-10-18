957fd18e011c0c682168c303092816bfcc307bd5
Sebastian Ullrich <kha@kha-arch.localdomain>
2011-10-18 17:24:26 +0200

    Default awesome config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,21 +6,18 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(removeKeys, additionalKeys)
 import System.IO
 
-main = do
-    xmproc <- spawnPipe "xmobar"
-    xmonad $ defaultConfig
-        { terminal = "xterm -fg White -bg Black"
-        , normalBorderColor = "#007f00#"
-        , focusedBorderColor = "#00ff00"
-        , logHook = fadeInactiveLogHook 0.9 >> 
-            dynamicLogWithPP xmobarPP
-                { ppOutput = hPutStrLn xmproc
-                , ppTitle = xmobarColor "green" "" . shorten 50
-                }
-        , manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , modMask = mod4Mask
-        }
-        `removeKeys` [(mod4Mask, xK_p)]
-        `additionalKeys` [((mod4Mask, xK_p), spawn "dmenu_run")]
-
+main =
+    let dzenArgs = "dzen2 -ta r -bg black -fg white" in
+let conf =
+            defaultConfig
+            { terminal = "xterm -fg White -bg Black"
+            , normalBorderColor = "#007f00#"
+            , focusedBorderColor = "#00ff00"
+            , logHook = fadeInactiveLogHook 0.9
+            , manageHook = manageDocks <+> manageHook defaultConfig
+            , layoutHook = avoidStruts  $  layoutHook defaultConfig
+            , modMask = mod4Mask
+            }
+            `removeKeys` [(mod4Mask, xK_p)]
+            `additionalKeys` [((mod4Mask, xK_p), spawn "dmenu_run")] in
+    xmonad =<< statusBar dzenArgs dzenPP (const (mod4Mask, xK_b)) conf
