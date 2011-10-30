20d20e6b076a952845a31b33ee4fbdbd8e636545
Sebastian Ullrich <sebasti@nullrich.de>
2011-10-30 20:29:03 +0100

    naughty kernel logs http://awesome.naquadah.org/wiki/Dbus,_naughty_and_logs

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -314,8 +314,8 @@ awful.rules.rules = {
     { rule = { class = "gimp" },
       properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
-    { rule = { class = "Firefox" },
-      properties = { tag = tags[1][2] } },
+--    { rule = { class = "Firefox" },
+--      properties = { tag = tags[1][2] } },
     { rule = { class = "Pidgin" },
       properties = { tag = tags[1][3] } },
     { rule = { class = "XTerm" },
