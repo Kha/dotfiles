03358ee0d33c1c5870f2f0e4a460bdc8e237ab66
Sebastian Ullrich <sebasti@nullri.ch>
2022-12-04 12:33:54 +0100

    doomPackageDir

--- nixpkgs/common.nix
+++ nixpkgs/common.nix
@@ -85,9 +85,31 @@ in {
     };
   };
 
-  programs.doom-emacs = {
+  programs.doom-emacs = rec {
     enable = true;
     doomPrivateDir = ../doom;
+    # Only init/packages so we only rebuild when those change.
+    doomPackageDir = let
+      filteredPath = builtins.path {
+        path = doomPrivateDir;
+        name = "doom-private-dir-filtered";
+        filter = path: type:
+          builtins.elem (baseNameOf path) [ "init.el" "packages.el" ];
+      };
+    in pkgs.linkFarm "doom-packages-dir" [
+      {
+        name = "init.el";
+        path = "${filteredPath}/init.el";
+      }
+      {
+        name = "packages.el";
+        path = "${filteredPath}/packages.el";
+      }
+      {
+        name = "config.el";
+        path = pkgs.emptyFile;
+      }
+    ];
     emacsPackagesOverlay = self: super: {
       inherit (lean4) lean4-mode;
     };
